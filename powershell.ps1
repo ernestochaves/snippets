dbac3852dbaa3c807d8238a1581946ebe8082c78
Ernesto <ernestochaveschaves@outlook.com>
2019-09-10 11:38:15 -0600

    build vs from ps

--- powershell.ps1
+++ powershell.ps1
@@ -1,5 +1,8 @@
 #Copy specific files from folder (minus the folder)
  Get-ChildItem C:\MyFolder\ -recurse -filter SomeFileName.* | Copy-Item -Destination C:\myfolderdest\
+ 
+#cool VS build from PS 
+#https://github.com/cknightdevelopment/KnightCodesExamples/tree/master/Miscellaneous/Build%20Solutions%20and%20Projects%20With%20PowerShell
 #Build a project: see https://knightcodes.com/miscellaneous/2016/09/05/build-solutions-and-projects-with-powershell.html 
 # nuget restore, clean solution, and build solution
 buildVS .\path\to\solution.sln
